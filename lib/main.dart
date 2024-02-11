2f2814655b2ac3d5b4c0dc60605a32e92768cae8
AntEdot <ante_larsson@live.se>
2024-02-11 13:11:17 +0100

    reactive text

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flame/game.dart';
@@ -77,6 +78,17 @@ class _MainWidgetState extends State<MainWidget> {
                                 onPressed: restart,
                                 child: const Text('Restart'),
                               ),
+                              const Expanded(
+                                child: SizedBox(
+                                  height: 100,
+                                  child: ReactiveText(
+                                    'ohjhjhsfgdkfghfdgbkfdg',
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 40),
+                                  ),
+                                ),
+                              )
                             ],
                           ),
                         ),
@@ -91,4 +103,87 @@ class _MainWidgetState extends State<MainWidget> {
       ),
     );
   }
+}
+
+class ReactiveText extends StatelessWidget {
+  const ReactiveText(this.text,
+      {super.key,
+      this.style,
+      this.maxLines,
+      this.maxHeight,
+      this.shouldRender});
+
+  final String text;
+  final TextStyle? style;
+  final int? maxLines;
+  final double? maxHeight;
+
+  final bool Function(bool didExceedMaxLines, bool didExceedMaxHeight)?
+      shouldRender;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: ReactiveTextPainter(
+        text: text,
+        style: style,
+        maxLines: maxLines,
+        maxHeight: maxHeight,
+        shouldRender: shouldRender ??
+            (didExceedMaxLines, didExceedMaxHeight) =>
+                !didExceedMaxLines && !didExceedMaxHeight,
+      ),
+    );
+  }
+}
+
+class ReactiveTextPainter extends CustomPainter {
+  final String text;
+  final TextStyle? style;
+  final int? maxLines;
+  final double? maxHeight;
+
+  final bool Function(bool didExceedMaxLines, bool didExceedMaxHeight)
+      shouldRender;
+
+  ReactiveTextPainter({
+    super.repaint,
+    required this.text,
+    this.style,
+    this.maxLines,
+    this.maxHeight,
+    required this.shouldRender,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final textSpan = TextSpan(text: text, style: style);
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      maxLines: maxLines,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    log(textPainter.height.toString());
+
+    if (shouldRender(textPainter.didExceedMaxLines,
+        textPainter.height > (maxHeight ?? size.height))) {
+      final xCenter = (size.width - textPainter.width) / 2;
+      final yCenter = (size.height - textPainter.height) / 2;
+      final offset = Offset(xCenter, yCenter);
+
+      textPainter.paint(canvas, offset);
+    }
+  }
+
+  @override
+  bool shouldRepaint(ReactiveTextPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(ReactiveTextPainter oldDelegate) => false;
 }
