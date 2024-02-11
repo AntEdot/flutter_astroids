78f826216852e347a83f4439beabe7e39c3d2ed2
Ante Larsson <ante@xtactor.com>
2024-02-11 16:34:22 +0100

    resize window rescales game

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'dart:developer';
+import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_astroids/game.dart';
+import 'package:window_manager/window_manager.dart';
 
 // https://docs.flame-engine.org/latest/tutorials/space_shooter/step_1.html
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  windowManager.setAspectRatio((1600 - 40) / 900);
+  windowManager.setMinimumSize(const Size(1600, 900));
+
   runApp(const MainWidget());
 }
 
@@ -18,10 +25,71 @@ class MainWidget extends StatefulWidget {
   State<MainWidget> createState() => _MainWidgetState();
 }
 
-class _MainWidgetState extends State<MainWidget> {
+class _MainWidgetState extends State<MainWidget> with WindowListener {
   SpaceShooterGame game = SpaceShooterGame();
 
+  double scale = 1;
+
   restart() => setState(() => game = SpaceShooterGame());
+
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+    Future.delayed(const Duration(milliseconds: 200), rescale);
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onWindowResized() {
+    super.onWindowResized();
+    rescale();
+  }
+
+  @override
+  void onWindowMaximize() {
+    super.onWindowMaximize();
+    Future.delayed(const Duration(milliseconds: 200), rescale);
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    super.onWindowUnmaximize();
+    Future.delayed(const Duration(milliseconds: 200), rescale);
+  }
+
+  @override
+  void onWindowEnterFullScreen() {
+    super.onWindowEnterFullScreen();
+    Future.delayed(const Duration(milliseconds: 200), rescale);
+  }
+
+  @override
+  void onWindowLeaveFullScreen() {
+    super.onWindowLeaveFullScreen();
+    Future.delayed(const Duration(milliseconds: 200), rescale);
+  }
+
+  @override
+  void onWindowMinimize() {
+    super.onWindowMinimize();
+    game.overlays.add('pauseMenu');
+    game.pauseEngine();
+  }
+
+  rescale() {
+    Size size = MediaQuery.of(context).size;
+    double widthScale = size.width / 1600;
+    double heightScale = (size.height) / 900;
+    setState(() {
+      scale = math.min(widthScale, heightScale).clamp(1, 2);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,161 +97,80 @@ class _MainWidgetState extends State<MainWidget> {
       theme: ThemeData.dark(),
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: GameWidget(
-          game: game,
-          overlayBuilderMap: {
-            'pauseMenu': (BuildContext context, SpaceShooterGame game) {
-              return ClipRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 8),
-                              child: Text(
-                                game.health > 0 ? 'Paused' : 'Game over',
-                                style: Theme.of(context).textTheme.displayLarge,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 24),
-                              child: Text(
-                                'Score: ${game.score}',
-                                style: Theme.of(context).textTheme.bodyLarge,
-                              ),
-                            ),
-                            if (game.health > 0)
-                              TextButton(
-                                onPressed: () => game.resumeEngine(),
-                                child: const Text('Resume'),
-                              ),
-                            TextButton(
-                              onPressed: restart,
-                              child: const Text('Restart'),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(60),
-                          child: Row(
+        body: Center(
+          child: Transform.scale(
+            scale: scale,
+            child: SizedBox(
+              height: 900,
+              width: 1600,
+              child: ClipRRect(
+                child: GameWidget(
+                  game: game,
+                  overlayBuilderMap: {
+                    'pauseMenu': (BuildContext context, SpaceShooterGame game) {
+                      return ClipRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                          child: Stack(
                             children: [
-                              TextButton(
-                                onPressed: restart,
-                                child: const Text('Restart'),
+                              Center(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 8),
+                                      child: Text(
+                                        game.health > 0 ? 'Paused' : 'Game over',
+                                        style: Theme.of(context).textTheme.displayLarge,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 24),
+                                      child: Text(
+                                        'Score: ${game.score}',
+                                        style: Theme.of(context).textTheme.bodyLarge,
+                                      ),
+                                    ),
+                                    if (game.health > 0)
+                                      TextButton(
+                                        onPressed: () => game.resumeEngine(),
+                                        child: const Text('Resume'),
+                                      ),
+                                    TextButton(
+                                      onPressed: restart,
+                                      child: const Text('Restart'),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              const Expanded(
-                                child: SizedBox(
-                                  height: 100,
-                                  child: ReactiveText(
-                                    'ohjhjhsfgdkfghfdgbkfdg',
-                                    maxLines: 1,
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 40),
+                              Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(60),
+                                  child: Row(
+                                    children: [
+                                      TextButton(
+                                        onPressed: () async {
+                                          windowManager.setFullScreen(!(await windowManager.isFullScreen()));
+                                        },
+                                        child: const Text('Fullscreen'),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              )
+                              ),
                             ],
                           ),
                         ),
-                      ),
-                    ],
-                  ),
+                      );
+                    },
+                  },
                 ),
-              );
-            },
-          },
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
-}
-
-class ReactiveText extends StatelessWidget {
-  const ReactiveText(this.text,
-      {super.key,
-      this.style,
-      this.maxLines,
-      this.maxHeight,
-      this.shouldRender});
-
-  final String text;
-  final TextStyle? style;
-  final int? maxLines;
-  final double? maxHeight;
-
-  final bool Function(bool didExceedMaxLines, bool didExceedMaxHeight)?
-      shouldRender;
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: ReactiveTextPainter(
-        text: text,
-        style: style,
-        maxLines: maxLines,
-        maxHeight: maxHeight,
-        shouldRender: shouldRender ??
-            (didExceedMaxLines, didExceedMaxHeight) =>
-                !didExceedMaxLines && !didExceedMaxHeight,
-      ),
-    );
-  }
-}
-
-class ReactiveTextPainter extends CustomPainter {
-  final String text;
-  final TextStyle? style;
-  final int? maxLines;
-  final double? maxHeight;
-
-  final bool Function(bool didExceedMaxLines, bool didExceedMaxHeight)
-      shouldRender;
-
-  ReactiveTextPainter({
-    super.repaint,
-    required this.text,
-    this.style,
-    this.maxLines,
-    this.maxHeight,
-    required this.shouldRender,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final textSpan = TextSpan(text: text, style: style);
-
-    final textPainter = TextPainter(
-      text: textSpan,
-      maxLines: maxLines,
-      textDirection: TextDirection.ltr,
-    );
-    textPainter.layout(
-      minWidth: 0,
-      maxWidth: size.width,
-    );
-
-    log(textPainter.height.toString());
-
-    if (shouldRender(textPainter.didExceedMaxLines,
-        textPainter.height > (maxHeight ?? size.height))) {
-      final xCenter = (size.width - textPainter.width) / 2;
-      final yCenter = (size.height - textPainter.height) / 2;
-      final offset = Offset(xCenter, yCenter);
-
-      textPainter.paint(canvas, offset);
-    }
-  }
-
-  @override
-  bool shouldRepaint(ReactiveTextPainter oldDelegate) => false;
-
-  @override
-  bool shouldRebuildSemantics(ReactiveTextPainter oldDelegate) => false;
 }
